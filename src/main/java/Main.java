cff26f5792885f94d4360481ab2c18e327cdcb88
annakosheleva <71738312+annakosheleva@users.noreply.github.com>
2023-07-14 15:41:29 -0400

    New Project init

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        driver.get("https://google.com");
+    }
+}
